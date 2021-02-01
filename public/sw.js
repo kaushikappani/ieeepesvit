e3d091ad928cfc7a53bae55d934f11c4445af3ba
info <58048398+kaushikappani@users.noreply.github.com>
2021-02-01 23:09:35 +0530

    sw

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open('static').then(cache => {
-            return cache.addAll(['./images/pes192x192.ICO','./images/ieee-pes-power-future2x.png'])
+            return cache.addAll(['./images/pes192x192.ico','./images/ieee-pes-power-future2x.png'])
         })
     )
 })
